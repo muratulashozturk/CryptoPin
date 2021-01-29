dff408a1eca2e37e12ebfda92c56d67a83586b2a
Murat Ulaş ÖZTÜRK <muratulasozturk@hotmail.com>
2021-01-29 23:41:45 +0300

    Add files via upload

---
+++ CryptoPin/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿
+namespace CryptoPin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.geckoWebBrowser1 = new Gecko.GeckoWebBrowser();
+            this.SuspendLayout();
+            // 
+            // geckoWebBrowser1
+            // 
+            this.geckoWebBrowser1.ConsoleMessageEventReceivesConsoleLogCalls = true;
+            this.geckoWebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.geckoWebBrowser1.Enabled = false;
+            this.geckoWebBrowser1.FrameEventsPropagateToMainWindow = false;
+            this.geckoWebBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.geckoWebBrowser1.Name = "geckoWebBrowser1";
+            this.geckoWebBrowser1.Size = new System.Drawing.Size(233, 112);
+            this.geckoWebBrowser1.TabIndex = 0;
+            this.geckoWebBrowser1.UseHttpActivityObserver = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(233, 112);
+            this.Controls.Add(this.geckoWebBrowser1);
+            this.Enabled = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Opacity = 0.3D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CryptoWin";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Gecko.GeckoWebBrowser geckoWebBrowser1;
+    }
+}
+
---
+++ CryptoPin/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Gecko;
+
+
+namespace CryptoPin
+{
+    public partial class Form1 : Form
+    {
+        string widgetHtml = "<html style='color:#4545F2;background-color:transparent;'> <!-- TradingView Widget BEGIN --> <style> .tv-ticker-item-last__title{ color:#4545F2; } span { border:none;color:#4545F2; } body { color:#4545F2;border:none; background-color: transparent; overflow: hidden; pointer-events: none;user-select: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none } div { color:#4545F2;border:none; background-color : transparent; overflow: hidden; pointer-events: none;user-select: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none } </style> <div class=\"tradingview-widget-container\"> <div class=\"tradingview-widget-container__widget\"></div> <script type=\"text/javascript\" src=\"https://s3.tradingview.com/external-embedding/embed-widget-single-quote.js\" async> { \"symbol\": \"BITSTAMP:BTCUSD\", \"width\": 505, \"colorTheme\": \"light\", \"isTransparent\": true, \"locale\": \"en\" } </script> </div> </html>";
+              
+
+        public Form1()
+        {
+            var screen = System.Windows.Forms.Screen.PrimaryScreen.Bounds;
+            var width = screen.Width;
+            var height = screen.Height;
+            var ustTaraf = height - (height - 200);
+            var altTaraf = height - 200;
+            Win32Api.SetWindowPosition(this.Handle,5, ustTaraf, 600, 100);
+            Win32Api.SetWindowTopmost(this.Handle, true);
+            Xpcom.Initialize(Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "Firefox64"));
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Application.UseWaitCursor = false;
+            this.Enabled = false;
+            Cursor.Hide();
+            Win32Api.ShowCursor(false);
+            geckoWebBrowser1.LoadHtml(widgetHtml);
+        }
+    }
+}
